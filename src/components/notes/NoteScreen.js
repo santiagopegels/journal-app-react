34e5733169917a593dd3f01b576a9f4923d1b530
Santiago Pegels <santiagopegels@gmail.com>
2021-06-22 16:28:32 -0300

    add notes styles

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,27 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                />
+
+                <textarea
+                    placeholder="What happened today?"
+                    className="notes__textarea"
+                >
+                </textarea>
+
+                <div className="notes__image">
+                    <img
+                        src="https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-810x540.jpg"
+                        alt="note-image"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
