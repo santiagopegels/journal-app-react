fc339f17f12d1acf18f27bc604a26698cacedca2
Santiago Pegels <santiagopegels@gmail.com>
2021-06-23 11:08:57 -0300

    Add redux

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux'
+import { authReducer } from '../reducers/authReducer'
+
+const reducers = combineReducers({
+    auth: authReducer
+})
+
+
+export const store = createStore(reducers)
