6a12b6cccedf26d7b3d1074e92dd22992cd72f28
Santiago Pegels <santiagopegels@gmail.com>
2021-06-30 11:16:21 -0300

    add create notes in firebase test

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -11,7 +11,21 @@ const firebaseConfig = {
     appId: "1:898529882843:web:abde72df858ff2db3cd2fe"
 };
 
-firebase.initializeApp(firebaseConfig);
+const firebaseConfigTesting = {
+    apiKey: "AIzaSyC0hPASIueL9jpN_1FaL6v52qr2-oppgq0",
+    authDomain: "testing-47aa0.firebaseapp.com",
+    projectId: "testing-47aa0",
+    storageBucket: "testing-47aa0.appspot.com",
+    messagingSenderId: "153805280086",
+    appId: "1:153805280086:web:0f63ca47efb3e67b273cf7"
+  };
+
+
+  if(process.env.NODE_ENV === 'test'){
+    firebase.initializeApp(firebaseConfigTesting);
+  } else{
+    firebase.initializeApp(firebaseConfig);
+  }
 
 const db = firebase.firestore()
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,9 +1,51 @@
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk' 
+import { startNewNote } from '../../actions/notes'
+import { types } from '../../types/types'
+import { db } from '../../firebase/firebase-config'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+const store = mockStore({
+    auth: {
+        uid: 'Testing',
+    }
+})
 
 describe('Test on notes actions', () => {
-    test('should ', () => {
-        
-    })
     
+    test('should create a new note', async () => {
+       
+        await store.dispatch(startNewNote())
+        
+        const actions = store.getActions()
 
+        expect(actions[0]).toEqual({
+            type: types.notesActive,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number)
+            }
+        })
+
+        expect(actions[1]).toEqual({
+            type: types.notesNewEntry,
+            payload: {
+                id: expect.any(String),
+                title: '',
+                body: '',
+                date: expect.any(Number)
+            }
+        })
+
+        //Delete from database
+        const docId = actions[0].payload.id
+        console.log(store.getState().auth.uid);
+        
+        await db.doc(`${store.getState().auth.uid}/journal/notes/${docId}`).delete()
+    })
 
 })
